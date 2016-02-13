c9bd6e8d19bd0e22c786f4c07adbd974a88d188f
xella29 <axelle.simon29@gmail.com>
2016-02-13 16:22:19 +0100

    Mise à jour css
    Maj page "A propos"

--- www/blog/wp-content/themes/blog-wprd/sidebar.php
+++ www/blog/wp-content/themes/blog-wprd/sidebar.php
@@ -1,6 +1,6 @@
 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?>
 	<img src="<?php echo home_url( '/wp-content/themes/blog-wprd/images/grand-pere.png' ); ?>" alt="Portrait du Grand-père" />
 
-	<p>Je m'appelle Leopold, et je suis un scientifique passioné à la retraite. Depuis tout jeune, le domaine de l'informatique me passione, et j'y ai consacré ma vie.<br/><br/>Ce carnet de bord a pour objectif de vous présenter plusieurs articles sur mon sujet de recherche : l'évolution des interfaces graphiques au cours de ces dernières années. J'espère ainsi vous permettre d'enrichir vos connaissances, et de rassasier vote curiosité !</p>
+	<p>Je m'appelle Leopold, et je suis un scientifique passionné à la retraite. Depuis tout jeune, le domaine de l'informatique me fascine, et j'y ai consacré ma vie.<br/><br/>Ce carnet de bord a pour objectif de vous présenter plusieurs articles sur mon sujet de recherche : l'évolution des interfaces graphiques au cours de ces dernières années. J'espère ainsi vous permettre d'enrichir vos connaissances, et d'éveiller votre curiosité !</p>
 
 <?php endif; ?>
