ba44258acc9334e4c468a2691ca0c6c12f36bd38
xella29 <axelle.simon29@gmail.com>
2016-02-22 16:54:13 +0100

    Up blog
    page 404 + css

--- www/blog/wp-content/themes/blog-wprd/404.php
+++ www/blog/wp-content/themes/blog-wprd/404.php
@@ -7,8 +7,7 @@
 		<div class="theNavigation"><b>Vous êtes ici</b> : <a href="<?php echo home_url( ); ?>">Accueil</a> / <strong>404 Page Not Found</strong></div>
 		<article>
 			<h1>Oops, la page n'existe pas ou plus !</h1>
-			<p>dsl bro ça existe po</p>
-			<div class="more"></div>
+			<p>Désolé, mais la page que vous recherchez n'existe pas ou plus. Vous pouvez essayer de rechercher une autre page avec le formulaire de recherche qui se trouve en haut de page. Sinon, vous pouvez aussi retourner à l'<a href="<?php echo home_url( ); ?>">accueil du blog</a>. Vous pouvez également visiter le <a href="http://www.enigma-tic.fr">site du projet</a>.</p>
 		</article>
 </section>
 <?php get_footer(); ?>
