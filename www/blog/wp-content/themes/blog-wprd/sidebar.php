bbc196385989dbd956956c3e962ec10b9a683232
xella29 <axelle.simon29@gmail.com>
2016-02-22 18:50:13 +0100

    up
    up

--- www/blog/wp-content/themes/blog-wprd/sidebar.php
+++ www/blog/wp-content/themes/blog-wprd/sidebar.php
@@ -1,6 +1,7 @@
 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?>
 	<img src="<?php echo home_url( '/wp-content/themes/blog-wprd/images/grand-pere.png' ); ?>" alt="Portrait du Grand-père" />
 
-	<p>Je m'appelle Léopold, et je suis un scientifique passionné à la retraite. Depuis tout jeune, le domaine de l'informatique me fascine, et j'y ai consacré ma vie.<br/><br/>Ce carnet de bord a pour objectif de vous présenter plusieurs articles sur mon sujet de recherche : l'évolution des interfaces graphiques au cours de ces dernières années. J'espère ainsi vous permettre d'enrichir vos connaissances, et d'éveiller votre curiosité !</p>
+	<p>Je m'appelle Léopold, et je suis un scientifique passionné à la retraite. Depuis tout jeune, le domaine de l'informatique me fascine, et j'y ai consacré ma vie.<br/><br/>Ce carnet de bord a pour objectif de vous présenter plusieurs articles sur mon sujet de recherche : l'évolution des interfaces graphiques au cours de ces dernières années. J'espère ainsi vous permettre d'enrichir vos connaissances, et d'éveiller votre curiosité !<br/><br/></p>
+	<p style="text-align: center;"><strong><?php if (function_exists('users_online')): ?><?php users_online(); ?><?php endif; ?></strong></p>
 
 <?php endif; ?>
