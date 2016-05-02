46619c0b2f6bbd3f6707645a96a01e3593f471c4
xella29 <axelle.simon29@gmail.com>
2016-05-02 21:25:51 +0200

    update analytics

--- www/siteTest/backend/views/layout/head-jeu.php
+++ www/siteTest/backend/views/layout/head-jeu.php
@@ -9,3 +9,12 @@
 <link rel="stylesheet" href="<?php echo STYLE_PATH?>stylesheet.css" type="text/css" media="screen" />
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,300,700|Roboto:400,300,500,700,300italic,400italic,100italic' rel='stylesheet' type='text/css'>
 <link rel="Shortcut Icon" href="<?php echo PICTURES_PATH?>vign.png" />
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-74188564-2', 'auto');
+  ga('send', 'pageview');
+</script>
